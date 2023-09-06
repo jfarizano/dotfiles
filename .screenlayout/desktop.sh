c00f120541ba3a63c5a44f2b64397d48192e670d
jfarizano <juanignaciofarizano@gmail.com>
2023-09-06 18:29:21 -0300

    default screen layout for desktop

--- .screenlayout/desktop.sh
+++ .screenlayout/desktop.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x103 --rotate normal --output HDMI-1 --off --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --mode 1920x1080 --pos 0x130 --rotate normal --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
+nitrogen --head=0 --set-scaled .config/nitrogen/jv408avutto91.jp
